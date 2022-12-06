dd040292fbdc9511e3cecaffb045438641019006
Cristian Garcia <117741045+Crostiandres@users.noreply.github.com>
2022-12-05 19:20:05 -0500

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,28 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function soloNumeros(array) {
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej: 
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+
+  // Tu código aca:
+  let nuevoarray = [];
+  for (i=0; i < array.length; i++){
+    if (Number.isInteger(array[i])){
+      nuevoarray.push(array[i]);
+    }
+  }
+return nuevoarray;
+  } 
+  
+  let array = [1, 'Hola', 2];
+  console.log (soloNumeros(array));
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = soloNumeros
